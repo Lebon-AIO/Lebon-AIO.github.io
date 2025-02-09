ef97bd9977ea8731fd34a1611d005ddf9f39d1f4
Tim <zqrk.pers@gmail.com>
2025-02-09 11:50:36 +0100

    Initial commit.

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="h-screen gap-2 flex flex-col items-center justify-center bg-neutral-900">
+      <h1 className="text-5xl md:text-7xl font-display bg-gradient-to-r from-violet-500 to-fuchsia-500 bg-clip-text text-transparent">
+        LebonAIO
+      </h1>
+      <p className="text-white/20 font-mono">
+        more informations soon
+      </p>
+    </div>
+
+  )
+}
+
+export default App
