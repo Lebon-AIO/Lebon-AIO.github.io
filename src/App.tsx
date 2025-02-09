eef309e66b7095224428246268bc019a8c4715e2
Tim <zqrk.pers@gmail.com>
2025-02-09 12:47:55 +0100

    Minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
 
   return (
-    <div className="h-screen gap-2 flex flex-col items-center justify-center bg-neutral-900">
+    <div className="h-screen gap-2 flex flex-col items-center justify-center bg-neutral-900 select-none cursor-cell">
       <h1 className="text-5xl md:text-7xl font-display bg-gradient-to-r from-violet-500 to-fuchsia-500 bg-clip-text text-transparent">
         LebonAIO
       </h1>
