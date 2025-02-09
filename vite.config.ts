fb57313af591697d185dcfbfd84e71dd8019efbd
Tim <zqrk.pers@gmail.com>
2025-02-09 12:37:56 +0100

    update homepage and adjust base URL for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Lebon-AIO.github.io/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
